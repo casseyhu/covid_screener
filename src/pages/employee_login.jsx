1af59e5dee7eb2e2877d7c4ee13c72bac713823e
huiga <eddiex2205@gmail.com>
2020-11-16 18:41:02 -0500

    employee login fix

--- src/pages/employee_login.jsx
+++ src/pages/employee_login.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import * as Constants from '../constants';
 
 
-const EmployeeLogin = () => {
-    return (
-        <div style={{ 'display':'flex', 'justifyContent':'center', 'alignItems':'center', 
-            'height':'100vh', 'flexDirection':'column', 'backgroundColor':Constants.BGCOLOR_GREEN}}>
-            <h1 style={{'margin':'10px'}}>Employee Login</h1>
-            <input class='loginInput' type='email' placeholder='email'></input>
-            <input class='loginInput' type='password' placeholder='password'></input>
-            <Link to="/employee/home" className="btn btn-outline-primary" style={{'margin':'5px'}}>Login</Link>
-        </div> 
-    )
+class EmployeeLogin extends Component {
+    
+    
+    
+    render(){
+        return (
+            <div style={{ 'display':'flex', 'justifyContent':'center', 'alignItems':'center', 
+                'height':'100vh', 'flexDirection':'column', 'backgroundColor':Constants.BGCOLOR_GREEN}}>
+                <h1 style={{'margin':'10px'}}>Employee Login</h1>
+                <form style={{ 'display':'flex', 'justifyContent':'center', 'alignItems':'center', 
+                'flexDirection':'column'}}>
+                    <input class='loginInput' type='email' placeholder='email'></input>
+                    <input class='loginInput' type='password' placeholder='password'></input>
+                    <Link to="/employee/home" className="btn btn-outline-primary" style={{'margin':'5px'}}>Login</Link>
+                </form>
+            </div> 
+        )
+    }
 }
 
 export default EmployeeLogin;
