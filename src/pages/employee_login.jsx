c0a1dbe2018d4632450d211fea4ff9c358286010
huiga <eddiex2205@gmail.com>
2020-11-21 02:23:56 -0500

    quickfix conflicts

--- src/pages/employee_login.jsx
+++ src/pages/employee_login.jsx
@@ -1,8 +1,4 @@
 import React, {Component} from 'react';
-<<<<<<< HEAD
-=======
-import { useForm } from 'react-hook-form';
->>>>>>> 735fc7af75d0b02c1267b56bd7fc288a68cb8055
 import * as Constants from '../constants';
 import axios from 'axios';
 
@@ -19,21 +15,6 @@ class EmployeeLogin extends Component {
     
     login = (event) => {
         event.preventDefault();
-<<<<<<< HEAD
-        console.log("Employee login in with credentials: ", this.state.email, this.state.password);
-        axios.get('/employee/login', {params: { email: this.state.email, 
-            pass: this.state.password }}).then(response => {
-                console.log("Query result is: ", response.data[0])
-                if(response.data === '' || response.data.length === undefined 
-                    || response.data.length === 0){
-                    console.log("memes")
-                }
-                else {
-                    // Login was successful, the return should be employee ID. How to load the 
-                    // employee_home page with the correct employee ID from here? 
-                    this.props.history.push('/employee/home')
-                }
-=======
         // console.log("Employee login in with credentials: ", this.state.email, this.state.password);
         axios.get('/employee/login', {params: { 
             email: this.state.email, 
@@ -51,7 +32,6 @@ class EmployeeLogin extends Component {
                     employeeID: response.data[0].employeeId
                 })
             }
->>>>>>> 735fc7af75d0b02c1267b56bd7fc288a68cb8055
         })
     }
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as Constants from '../constants';
 
-<<<<<<< HEAD
 
 const MainPage = () => {
     return (
@@ -15,19 +14,6 @@ const MainPage = () => {
             </div>
         </div>        
     )
-=======
-class MainPage extends Component {
-    render() { 
-        return ( 
-            <div style={{ 'display':'flex', 'justifyContent':'center', 'alignItems':'center', 
-            'height':'100vh', 'flexDirection':'column', 'backgroundColor':Constants.BGCOLOR_MILKCOFFEE}}>
-            <h1 style={{'margin':'10px'}}>I AM ...</h1>
-            <Link to="/labtech" className="btn btn-outline-primary" style={{'margin':'10px', 'width':'150px'}}>Lab Worker</Link>
-            <Link to="/employee" className="btn btn-outline-primary" style={{'margin':'10px', 'width':'150px'}}>Employee</Link>
-        </div>      
-         );
-    }
->>>>>>> 735fc7af75d0b02c1267b56bd7fc288a68cb8055
 }
  
 export default MainPage;
