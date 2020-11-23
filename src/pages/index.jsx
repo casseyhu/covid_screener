c0bbe3e71b16d27fb8fcf4a8f0d282ac7aad62bf
casseyhu <casseyhu123@gmail.com>
2020-11-22 21:27:09 -0500

    add background images

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,7 +7,8 @@ class MainPage extends Component {
 
     render() {
         return (
-            <div className='verticalFlex' style={{ 'height':'100vh', 'backgroundColor':Constants.BGCOLOR_MILKCOFFEE}}>
+            <div className='verticalFlex main-container' style={{ height:'100vh', backgroundColor:Constants.BGCOLOR_MILKCOFFEE}}>
+                <div className='main background'></div>
                 <div className="loginBox">
                     <h1 style={{'margin':'10px'}}>I AM ...</h1>
                     <Link to="/labtech" className="btn btn-outline-primary" style={{'margin':'10px', 'width':'130px'}}>Lab Worker</Link>
